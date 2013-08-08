d258c765850ef42275d210f0e52f31f4672a3831
Nick Charlton <hello@nickcharlton.net>
2013-08-08 11:03:49 +0100

    Adds docstrings and error handling to the Sinatra bit.

--- app.rb
+++ app.rb
@@ -1,6 +1,35 @@
 require 'sinatra'
+require 'json'
 
+#
+# Routes
+#
+
+#
+# Documentation
+#
 get '/' do
   erb :home
+end
+
+#
+# Find Nearby Cinemas by Postcode
+#
+get '/cinemas/find/:postcode' do
+  
+end
+
+#
+# Show Listings for Cinemas
+#
+get '/cinemas/:venue_id/showings' do
+
+end
+
+#
+# Error Handling
+#
+not_found do
+  {:status => 404, :message => "Not Found"}.to_json
 end
 
