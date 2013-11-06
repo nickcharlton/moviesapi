0fd759c5ca7a9e6f952c9fe043d96d6e5c9c17c9
Nick Charlton <hello@nickcharlton.net>
2013-11-06 17:26:34 +0000

    Adds link headers to aid in pagination.

--- app.rb
+++ app.rb
@@ -58,6 +58,23 @@ get '/cinemas/find/:postcode' do
     halt 400, {:message => 'The specified page parameter was out of range.'}.to_json
   end
 
+  # set the link headers
+  total = cinemas[:total]
+  next_page_count = params[:page] + 1
+  last_page_count = (total.to_f / increment.to_f).round
+  
+  next_page = url('/cinemas/find/%s?page=%d' % [params[:postcode], next_page_count])
+  last_page = url('/cinemas/find/%s?page=%d' % [params[:postcode], last_page_count])
+
+  # next isn't helpful unless we can progress
+  if next_page_count > last_page_count
+    link_set = "<#{last_page}>; rel='last'"
+  else
+    link_set = "<#{next_page}>; rel='next', <#{last_page}>; rel='last'"
+  end
+
+  headers 'Link' => link_set
+
   #  but, if it all worked, pass the data
   cinemas[:cinemas].to_json
 end
